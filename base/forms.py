2a20a000a5302f599027ab83c38a22346ff81c87
Nick Santiago <nicholasstgo@gmail.com>
2022-10-16 15:56:59 -0500

    Bootstrap

--- base/forms.py
+++ base/forms.py
@@ -2,8 +2,8 @@ from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
 from django import forms
 
 class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length = 50, label='Your name')
-    last_name = forms.CharField(max_length = 50, label='Your last name')
-    email_address = forms.EmailField(max_length = 150, label='Your email adress')
+    first_name = forms.CharField(max_length = 50, label='Name')
+    last_name = forms.CharField(max_length = 50, label='Last Name')
+    email_address = forms.EmailField(max_length = 150, label='Email')
     message = forms.CharField(max_length = 1000)
 
