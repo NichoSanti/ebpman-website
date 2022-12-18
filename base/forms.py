ab593316b2c4b7fd2d60434cf7ef9934bed07eaa
Milton N Santiago <MiltonNSantiago@gmail.com>
2022-12-17 21:36:29 -0600

    Worked some more on contact form/model

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,8 @@
 from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
+from django.forms import ModelForm
 from django import forms
 from .models import Contact
+
 
 # class ContactForm(forms.Form):
 #     first_name = forms.CharField(max_length = 50, label='Your name')
@@ -8,9 +10,10 @@ from .models import Contact
 #     email_address = forms.EmailField(max_length = 150, label='Your email adress')
 #     message = forms.CharField(max_length = 1000)
 
-def ContactForm(ModelForm):
-    class Meta:
+
+class ContactForm(ModelForm):
+     class Meta:
         model = Contact
-        field = '__all__'
-        
+        fields = '__all__'
+
 
--- base/models.py
+++ base/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Contact(models.Model):
-    email = models.EmailField()
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
+    first_name = models.CharField(max_length = 50, label='Your name')
+    last_name = models.CharField(max_length = 50, label='Your last name')
+    email_address = models.EmailField(max_length = 150, label='Your email adress')
+    message = models.CharField(max_length = 1000)
 
     def __str__(self):
         return self.email
