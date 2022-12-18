320ab372a75765a3af802baef157204ec615537b
Milton N Santiago <MiltonNSantiago@gmail.com>
2022-12-17 21:55:46 -0600

    Changed model data and migrated. Also changed some words in contact page

--- base/models.py
+++ base/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length = 50, label='Your name')
-    last_name = models.CharField(max_length = 50, label='Your last name')
-    email_address = models.EmailField(max_length = 150, label='Your email adress')
-    message = models.CharField(max_length = 1000)
+    first_name = models.CharField(max_length = 50, default='test first name')
+    last_name = models.CharField(max_length = 50, default='test last name')
+    email_address = models.EmailField(max_length = 150, default='test email')
+    message = models.CharField(max_length = 1000, default='test message')
 
     def __str__(self):
         return self.email
