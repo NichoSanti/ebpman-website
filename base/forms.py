d3f1991f12d7b5d9a3f8ca12c23db2d914218092
Milton N Santiago <MiltonNSantiago@gmail.com>
2022-12-17 21:05:24 -0600

    Created contact form and model

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,16 @@
 from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
 from django import forms
+from .models import Contact
 
-class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length = 50, label='Your name')
-    last_name = forms.CharField(max_length = 50, label='Your last name')
-    email_address = forms.EmailField(max_length = 150, label='Your email adress')
-    message = forms.CharField(max_length = 1000)
+# class ContactForm(forms.Form):
+#     first_name = forms.CharField(max_length = 50, label='Your name')
+#     last_name = forms.CharField(max_length = 50, label='Your last name')
+#     email_address = forms.EmailField(max_length = 150, label='Your email adress')
+#     message = forms.CharField(max_length = 1000)
+
+def ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        field = '__all__'
+        
 
