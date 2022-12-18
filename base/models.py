29a398cc0fb8efeb28681240be81809ffb852d57
Milton N Santiago <MiltonNSantiago@gmail.com>
2022-12-17 20:46:42 -0600

    Started work on a new contact form

--- base/models.py
+++ base/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Contact(models.Model):
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
