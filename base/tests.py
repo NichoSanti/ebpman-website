4f7bc9c929f57567958d010c17b40ce529cd091e
Milton N Santiago <MiltonNSantiago@gmail.com>
2022-12-18 19:43:35 -0600

    started work on tests for contact form

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .forms import ContactForm
 
-# Create your tests here.
+class ContactFormTest(TestCase):
+    def setUp(self):
+        form_data = {
+            'first_name': 'John', 
+            'last_name': 'Smith', 
+            'email_address': 'johnsmith@gmail.com',
+            'message': 'This is a test message',
+            } 
+        form = ContactForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    
+      
+
