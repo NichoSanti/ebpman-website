c263e7d74c8acdf77c28a08a6636ebc4b0412ecf
Milton N Santiago <MiltonNSantiago@gmail.com>
2022-12-20 19:53:01 -0600

    worked on tests

--- base/tests.py
+++ base/tests.py
@@ -2,14 +2,14 @@ from django.test import TestCase
 from .forms import ContactForm
 
 class ContactFormTest(TestCase):
-    def setUp(self):
+    def test_form_validity(self):
         form_data = {
             'first_name': 'John', 
             'last_name': 'Smith', 
             'email_address': 'johnsmith@gmail.com',
             'message': 'This is a test message',
             } 
-        form = ContactForm(data=form_data)
+        form = ContactForm(form_data)
         self.assertTrue(form.is_valid())
     
       
