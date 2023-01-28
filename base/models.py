b2b6c8ba4e522f8ed146ee0f3445216713155324
Milton N Santiago <MiltonNSantiago@gmail.com>
2023-01-28 01:13:16 -0600

    changed from SQLite to PostgreSql

--- base/models.py
+++ base/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length = 50, null=True)
-    last_name = models.CharField(max_length = 50, null=True)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
     email_address = models.EmailField(max_length = 150, null=True)
-    message = models.TextField(max_length = 1000, null=True)
+    message = models.TextField(max_length = 1000)
     created = models.DateTimeField(auto_now_add=True, null=True)
 
     class Meta:
         ordering = ['-created']
 
     def __str__(self):
-        return self.first_name
+        return self.email_address, self.message[0:50]
--- ebpmanwebsite/settings.py
+++ ebpmanwebsite/settings.py
@@ -12,19 +12,30 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -81,8 +92,10 @@ WSGI_APPLICATION = 'ebpmanwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD':env('PASSWORD'),
     }
 }
 
