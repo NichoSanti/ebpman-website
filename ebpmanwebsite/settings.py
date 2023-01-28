3eef52cd91980c92ccc57f8262b8c0ac5fdb8e76
Milton N Santiago <MiltonNSantiago@gmail.com>
2023-01-28 16:28:06 -0600

    fixed spacing

--- ebpmanwebsite/settings.py
+++ ebpmanwebsite/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('NAME'),
         'USER': env('USER'),
-        'PASSWORD':env('PASSWORD'),
+        'PASSWORD': env('PASSWORD'),
     }
 }
 
