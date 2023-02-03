2c81d2ce1bab0d28ff47322a8a456250d56d1a57
Milton N Santiago <MiltonNSantiago@gmail.com>
2023-02-02 20:07:16 -0600

    fixed contact model

--- base/models.py
+++ base/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length = 50)
-    last_name = models.CharField(max_length = 50)
-    email_address = models.EmailField(max_length = 150, null=True)
-    message = models.TextField(max_length = 1000)
-    created = models.DateTimeField(auto_now_add=True, null=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=150, null=True)
+    message = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
     
 
     class Meta:
